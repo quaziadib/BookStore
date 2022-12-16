3214303875aa3558ca25bcee73ffb3f41921c3cf
quaziadib <quaziadib@gmail.com>
2022-12-16 20:16:53 +0600

    hudai

---
+++ author_login_view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>.:: The Book Store ::.</title>
+</head>
+<link rel="stylesheet" href="login.css">
+<body>
+    <div class="loginbox">
+    <img src="user_icon.png" class="user_icon">
+        <h1>Login</h1>
+        <form>
+            <p>Username</p>
+            <input type="text" name="" id="" placeholder="Enter Username" action="" post="">
+            <p>Password</p>
+            <input type="password" name="" id="" placeholder="Enter Password">
+            <br>
+            <input type="submit" name="" value="Login">
+            <a href="#">New Here? Click to sign up</a>
+        </form>
+    </div>
+</body>
+</html>
