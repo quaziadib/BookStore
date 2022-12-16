797a14e7a09ce3f403a8e7aa55253c50fd8b6816
mollahmdsaif <64201131+mollahmdsaif@users.noreply.github.com>
2022-12-16 20:11:41 +0600

    Update customer_login.php

--- customer_login.php
+++ customer_login.php
@@ -7,22 +7,22 @@ if(isset($_POST['fname']) && isset($_POST['pass'])){
 	// write the query to check if this username and password exists in our database
 	$u = $_POST['fname'];
 	$p = $_POST['pass'];
-	$sql = "SELECT * FROM Customer WHERE user_id = '$u' AND Cpassword = '$p'";
+	$sql = "SELECT * FROM customer WHERE user_id = '$u' AND Cpassword = '$p'";
 	
 	//Execute the query 
 	$result = mysqli_query($conn, $sql);
 	
 	//check if it returns an empty set
-    /*
+    
 	if(mysqli_num_rows($result) !=0 ){
 	
-		//echo "LET HIM ENTER";
-		header("Location: home.php");
+		echo "LET HIM ENTER";
+		//header("Location: home.php");
 	}
 	else{
-		//echo "Username or Password is wrong";
-		header("Location: index.php");
-	}*/
+		echo "Username or Password is wrong";
+		//header("Location: index.php");
+	}
 	
 }
 
