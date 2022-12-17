bcdec5a1e958a7b5cb16d0150ae7fefed932bd23
quaziadib <quaziadib@gmail.com>
2022-12-17 10:51:03 +0600

    maaff chai

--- author_signup.php
+++ author_signup.php
@@ -3,15 +3,16 @@
 require_once('DBconnect.php');
 
 // we need to check if the input in the form textfields are not empty
-if(isset($_POST['author_id']) && isset($_POST['Wname']) && isset($_POST['Wemail']) && isset($_POST['Wpassword']) && isset($_POST['Plicense_no']) && isset($_POST['Pbank_ac_no'])){
+if(isset($_POST['author_id']) && isset($_POST['Wname']) && isset($_POST['Wemail']) && isset($_POST['Wpassword'])){
 	// write the query to check if this username and password exists in our database
 	$author_id = $_POST['author_id']
 	$Wname = $_POST['Wname'];
     $Wemail = $_POST['Wemail']
 	$Wpassword = $_POST['Wpassword'];
+	$admin_id = $_POST['Wpassword'];
 
 	
-	$sql = " INSERT INTO student VALUES( '$author_id', '$Wname', '$Wemail', '$Wpassword');";
+	$sql = " INSERT INTO student VALUES( '$author_id', '$Wname', '$Wemail', '$Wpassword', '$admin_id');";
 	
 	//Execute the query 
 	$result = mysqli_query($conn, $sql);
---
+++ author_signup_view.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>.:: The Book Store ::.</title>
+</head>
+<link rel="stylesheet" type="text/css" href="css/login.css">
+<body>
+
+
+    <div class="loginbox">
+    <img src="img/user_icon.png" class="user_icon">
+        <h1>Customer Signup</h1>
+        <form action="author_signup.php" method="post">
+            <p>Author ID</p>
+            <input type="text" name="author_id" id="" placeholder="Enter Author ID" >
+            <p>Full Name</p>
+            <input type="text" name="Wname" id="" placeholder="Enter Name" >
+            <p>Email</p>
+            <input type="text" name="Wemail" id="" placeholder="Enter Email" >
+            <p>Password</p> 
+            <input type="password" name="Wpassword" id="" placeholder="Enter Password">
+            
+            <br>
+            <input type="submit" name="" value="Signup">
+            
+            
+        </form>
+    
+
+    </div>
+</body>
+</html>
