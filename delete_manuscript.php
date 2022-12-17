26da6ef63d178471d0dae0d51572905375357555
mollahmdsaif <64201131+mollahmdsaif@users.noreply.github.com>
2022-12-17 12:22:34 +0600

    main

---
+++ delete_manuscript.php
@@ -0,0 +1,28 @@
+<?php
+// first of all, we need to connect to the database
+require_once('DBconnect.php');
+
+
+// we need to check if the input in the form textfields are not empty
+if(isset($_POST['pub_id']) && isset($_POST['author_id']) ){
+	// write the query to check if this username and password exists in our database
+	$b = $_POST['pub_id'];
+    $a = $_POST['author_id'];
+	$sql = "DELETE FROM Send_Manuscript WHERE pub_id='$b';";
+	
+	//Execute the query 
+	$result = mysqli_query($conn, $sql);
+	
+	
+	//check if it returns an empty set
+    
+	if($result){
+		header("Location: publisher_ad_view.php");
+	}
+	else{
+		echo "Project Failed! 404!";
+		//header("Location: index.php");
+	}
+	
+}
+?>
---
+++ publisher_ad_view.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/admin_ad_view.css">
+</head>
+<body>
+    <header>
+        <a href="" class="logo">The Book Store</a>
+        <ul>
+            <li><a href="books_ad_view.php">Books</a></li>
+            <li><a href="admin_ad_view.php">Admin</a></li>
+            <li><a href="author_ad_view.php">Author</a></li>
+            <li><a href="publisher_ad_view.php">Publisher</a></li>
+            <li><a href="customer_ad_view.php">Customer</a></li>
+            <li><a href="admin_login_view.php">Logout</a></li>
+            </div>
+        </ul>
+
+    </header>
+
+
+    
+    <section class="banner"></section>
+    <div id="main">
+
+        <div class="tile">
+            <div>
+                <p style="text-align: center">Delete Manuscript</p>
+            </div>
+            <div style="text-align: center">
+                <form action="delete_manuscript.php" method="post" style="padding-top: 10px;">
+                <input type="text" name="pub_id" id="" placeholder="Enter Publisher ID" >
+                <input type="text" name="author_id" id="" placeholder="Enter Author ID" >
+                <br>
+                <input type="Submit" name="" value="Submit">
+                </form>
+            </div>
+        </div>
+
+        <br>
+
+        <div class="tile">
+            <div>
+                <p style="text-align: center">Add Manuscript</p>
+            </div>
+            <div style="text-align: center">
+            <form action="add_req.php" method="post" style="padding-top: 10px;">
+                <input type="text" name="pub_id" id="" placeholder="Enter Publishers ID" >
+                <input type="text" name="author_id" id="" placeholder="Enter Your ID" >
+                
+                <br>
+                <br>
+                <input type="Submit" name="" value="Submit">
+                </form>
+            </div>
+        </div>
+
+        <br>
+
+        <div class="tile">
+            <div>
+                <p style="text-align: center">Modify Manuscript</p>
+            </div>
+            <div style="text-align: center">
+                <form action="modify_manuscript.php" method="post" style="padding-top: 10px;">
+                <input type="text" name="pub_id" id="" placeholder="Enter Publishers ID" >
+                <input type="text" name="author_id" id="" placeholder="Enter Your ID" >
+                
+                <br>
+                <br>
+                <input type="Submit" name="" value="Submit">
+                </form>
+                <span></span>
+            </div>
+        </div>
+
+        <hr>
+
+        
+
+        <?php 
+        require_once("DBconnect.php");
+        $sql = "SELECT * FROM book";
+        $result = mysqli_query($conn, $sql);
+        if(mysqli_num_rows($result) > 0){
+            //here we will print every row that is returned by our query $sql
+            while($row = mysqli_fetch_array($result)){
+            //here we have to write some HTML code, so we will close php tag
+        ?>
+        <div class="tile">
+            <div class="tile_text_area">
+                <p class="book_id">Book ID: <?php echo $row[0]; ?></p><br>
+                <p class="book_name">Name: <?php echo $row[1]; ?></p><br>
+                <p class="book_publish_date">Published Year: <?php echo $row[2]; ?></p><br>
+                <p class="book_price">Price: <?php echo $row[3]; ?></p>
+            </div>
+        </div>
+        
+        <?php 
+            }					
+        }
+        ?>
+
+    </div>
+
+    
+   
+<script>
+    let subMenu = document.getElementByID("subMenu");
+    function toggleMenu(){
+        subMenu.classList.toggle("open-menu");
+    }
+</script>
+
+</body>
+</html>
